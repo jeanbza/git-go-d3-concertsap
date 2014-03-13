a3c2b027503f0e822437ccaae13f9662264fd7ae
marcusmolchany <marcusmolchany@gmail.com>
2014-03-12 20:22:39 -0400

    removed about, blog and contact and removed corresponding code in main.go

--- main.go
+++ main.go
@@ -18,12 +18,7 @@ type Post struct {
 }
 
 func main() {
-    http.HandleFunc("/", blogPage)
-    http.HandleFunc("/blog", blogPage)
-    http.HandleFunc("/blog/", blogPage)
-
-    http.HandleFunc("/about", aboutPage)
-    http.HandleFunc("/about/", aboutPage)
+    http.HandleFunc("/", contactPage)
 
     http.HandleFunc("/contact", contactPage)
     http.HandleFunc("/contact/", contactPage)
@@ -39,41 +34,6 @@ func main() {
 
     err := http.ListenAndServe(":8080", nil)
     checkError(err)
-}
-
-func blogPage(rw http.ResponseWriter, req *http.Request) {
-    p := Page{
-        Title: "blog",
-        Posts: []Post{
-            Post{
-                Title: "Test blog title 1",
-                Content: "Test blog content 1",
-            },
-            Post{
-                Title: "Test blog title 2",
-                Content: "Test blog content 2",
-            },
-            Post{
-                Title: "Test blog title THE LAST",
-                Content: "Test blog content BOOM THE LAST",
-            },
-        },
-    }
-
-    tmpl := make(map[string]*template.Template)
-    tmpl["blog.html"] = template.Must(template.ParseFiles("html/blog.html", "html/index.html"))
-    tmpl["blog.html"].ExecuteTemplate(rw, "base", p)
-}
-
-func aboutPage(rw http.ResponseWriter, req *http.Request) {
-    p := Page{
-        Title: "about",
-        Posts: nil,
-    }
-
-    tmpl := make(map[string]*template.Template)
-    tmpl["about.html"] = template.Must(template.ParseFiles("html/about.html", "html/index.html"))
-    tmpl["about.html"].ExecuteTemplate(rw, "base", p)
 }
 
 func contactPage(rw http.ResponseWriter, req *http.Request) {
