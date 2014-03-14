9a3cb5b3b54ef7f3536897700679b1b87ff8704e
marcusmolchany <marcusmolchany@gmail.com>
2014-03-14 01:30:05 -0400

    added function to handle input from sqlEntry (doesn't work yet)

--- src/sqlEntry/sqlEntry.go
+++ src/sqlEntry/sqlEntry.go
@@ -3,6 +3,7 @@ package sqlEntry
 import (
     "net/http"
     "html/template"
+    "fmt"
 )
 
 type Page struct {
@@ -17,4 +18,18 @@ func GetPage(rw http.ResponseWriter, req *http.Request) {
     tmpl := make(map[string]*template.Template)
     tmpl["sqlEntry.html"] = template.Must(template.ParseFiles("static/html/sqlEntry.html", "static/html/index.html"))
     tmpl["sqlEntry.html"].ExecuteTemplate(rw, "base", p)
+}
+
+func submitHandler(rw http.ResponseWriter, req *http.Request) {
+    concertID := req.FormValue("concertID")
+    retailerID := req.FormValue("retailerID")
+    price := req.FormValue("price")
+    timestamp := req.FormValue("timestamp")
+
+    fmt.Printf(concertID)
+    fmt.Printf(retailerID)
+    fmt.Printf(price)
+    fmt.Printf(timestamp)
+
+    http.Redirect(rw, req, "/sqlEntry", http.StatusFound)
 }
