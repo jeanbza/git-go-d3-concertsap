7c282cebb5576c933adab4c1777c6f0b4230370a
jadekler <jadekler@gmail.com>
2014-03-13 21:50:44 -0400

    Added database insert

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
     "git-go-d3-concertsap/src/home"
-    "git-go-d3-concertsap/src/common"
     "git-go-d3-concertsap/src/database"
-    "log"
     "net/http"
 )
 
@@ -14,18 +12,7 @@ func main() {
     fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
     http.Handle("/static/", fileServer)
 
-    var (
-        id int
-        acronym string
-    )
-
-    rows := database.Select("SELECT id,acronym FROM state", "concertsap")
-
-    for rows.Next() {
-        err := rows.Scan(&id, &acronym)
-        log.Println(id, acronym)
-        common.CheckError(err)
-    }
+    database.Insert("INSERT INTO state (name, acronym) VALUES ('Virginia', 'VA'), ('Washington', 'WA'), ('Arizona', 'AZ'), ('Colorado', 'CO')", "concertsap")
 
     // err := http.ListenAndServe(":8080", nil)
     // common.CheckError(err)
--- src/database/connect.go
+++ src/database/connect.go
@@ -8,6 +8,7 @@ import (
 
 func Select(query string, database string) (*sql.Rows) {
     databaseDetails := common.StrCat("root:@tcp(127.0.0.1:3306)/", database)
+    
     db, err := sql.Open("mysql", databaseDetails)
     common.CheckError(err)
 
@@ -19,6 +20,10 @@ func Select(query string, database string) (*sql.Rows) {
 
 func Insert(query string, database string) {
     databaseDetails := common.StrCat("root:@tcp(127.0.0.1:3306)/", database)
+    
     db, err := sql.Open("mysql", databaseDetails)
-    common.CheckError(err)   
+    common.CheckError(err)
+    
+    _, err = db.Query(query) 
+    common.CheckError(err)
 }
