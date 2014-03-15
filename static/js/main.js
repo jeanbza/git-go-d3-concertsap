550da20a9f36038bf922e081122f4cf4c32192f8
jadekler <jadekler@gmail.com>
2014-03-14 22:04:20 -0400

    Added jquery ui. Added require.js. Modularized js into main.js

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+require.config({
+    baseUrl: '/static/js/',
+    urlArgs: "bust=" + (new Date()).getTime(),
+    paths: {
+        'jquery': 'external/jquery/jquery-2.0.3.min',
+        'jqueryui': 'external/jquery-ui/jquery-ui-1.10.4.custom',
+        'bootstrap': 'external/bootstrap/bootstrap.min',
+        'bootstrap-dtp': 'external/bootstrap/bootstrap-datetimepicker.min'
+    },
+
+    shim: {  
+        "jqueryui": {
+            deps: ['jquery']
+        },
+        "bootstrap" : {
+            deps: ['jquery']
+        },
+        'bootstrap-dtp': {
+            deps: ['jquery', 'bootstrap']
+        }
+    }
+});
+
+require([
+    'jquery'
+], 
+function ($) {
+    $(document).ready(function() {
+        console.dir("boom");
+    });
+});
