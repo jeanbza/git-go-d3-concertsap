a9f4c44b830d8170dffba72177baa32d4ff53b4c
jadekler <jadekler@gmail.com>
2014-03-16 17:26:44 -0400

    Gave home its own folder

--- app/home/home.go
+++ app/home/home.go
@@ -17,6 +17,6 @@ func GetPage(rw http.ResponseWriter, req *http.Request) {
     }
 
     tmpl := make(map[string]*template.Template)
-    tmpl["home.html"] = template.Must(template.ParseFiles("templates/home.html", "templates/index.html"))
+    tmpl["home.html"] = template.Must(template.ParseFiles("templates/home/home.html", "templates/index.html"))
     tmpl["home.html"].ExecuteTemplate(rw, "base", p)
 }
