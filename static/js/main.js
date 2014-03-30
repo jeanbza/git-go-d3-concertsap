476bf59343b468513b0a6d8c02038df035d7a355
jadekler <jadekler@gmail.com>
2014-03-30 00:18:46 -0400

    Added working timepicker to band concert record

--- static/js/main.js
+++ static/js/main.js
@@ -2,21 +2,25 @@ require.config({
     baseUrl: '/static/js/',
     urlArgs: "bust=" + (new Date()).getTime(),
     paths: {
-        'jquery': 'external/jquery/jquery-2.0.3.min',
+        'jquery': 'external/jquery/jquery-2.1.0',
         'jqueryui': 'external/jquery-ui/jquery-ui-1.10.4.custom',
         'bootstrap': 'external/bootstrap/bootstrap.min',
-        'bootstrap-dtp': 'external/bootstrap/bootstrap-datetimepicker.min'
+        'bootstrap-dtp': 'external/bootstrap/bootstrap-datetimepicker.min',
+        'moment': 'external/moment/moment.min'
     },
 
     shim: {  
         "jqueryui": {
             deps: ['jquery']
         },
-        "bootstrap" : {
+        "bootstrap": {
             deps: ['jquery']
         },
-        'bootstrap-dtp': {
-            deps: ['jquery', 'bootstrap']
+        "moment": {
+            deps: ['jquery']
+        },
+        "bootstrap-dtp": {
+            deps: ['jquery', 'bootstrap', 'moment']
         }
     }
 });
@@ -24,11 +28,24 @@ require.config({
 require([
     'jquery',
     'bootstrap',
-    'modules/parser'
+    'modules/parser',
+    'bootstrap-dtp'
 ], 
 function ($) {
     $(document).ready(function() {
         $('.dropdown-toggle').dropdown();
+
+        $('.cs-js-datetimepicker').datetimepicker({
+            icons: {
+                time: "fa fa-clock-o",
+                date: "fa fa-calendar",
+                up: "fa fa-arrow-up",
+                down: "fa fa-arrow-down"
+            }
+        }).on("dp.change",function (e) {
+            $("input.cs-js-date").val($(this).data("DateTimePicker").getDate().format("YYYY-MM-DD"));
+            $("input.cs-js-time").val($(this).data("DateTimePicker").getDate().format("hh:mm:ss"));
+        });;
 
         $(".cs-js-parse-input").parser({
             elemDisplay: $(".cs-js-parse-display"),
