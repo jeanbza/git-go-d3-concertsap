8e38dbb3a3264a3f1605d6e2973f9ca9623e5e7f
marcusmolchany <marcusmolchany@gmail.com>
2014-04-02 19:52:32 -0400

    moved config.rb to the project root directory

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "static/css"
+sass_dir = "static/sass"
+images_dir = "static/img"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
